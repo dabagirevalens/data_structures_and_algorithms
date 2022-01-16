45c5fa66fb061c439f1e7305603aba43950670e7
dabagirevalens <vdabagire@gmail.com>
2022-01-16 11:14:20 +0200

    fibonacci of n

---
+++ recursion/factorial.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    
+    if(n==0){
+        return 1;
+    }
+
+    int fact = n * factorial(n-1);
+
+    return fact;
+}
+
+int main(){
+
+    cout << "enter any number." << endl;
+    int n ;
+    cin >> n;
+
+    cout << factorial(n) << endl;
+
+    return 0;
+}
